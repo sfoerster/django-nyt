2af69907429d82a010c6f09429f0a2f0ed1103e2
Steven Foerster <sfoerster@gmail.com>
2020-06-07 00:30:52 -0400

    initial channels 2 support

--- django_nyt/consumers.py
+++ django_nyt/consumers.py
@@ -1,13 +1,35 @@
 import logging
 
-from channels import Group
-from channels.auth import channel_session_user, channel_session_user_from_http
-
 from . import models, settings
 
 logger = logging.getLogger(__name__)
 
+try:
+    channels_ver = "1"
+    from channels import Group
+    from channels.auth import channel_session_user, channel_session_user_from_http
+except Exception as e:
+    logger.info("Not using channels 1. Attempting channels 2...")
+    channels_ver = "2"
+    from channels.generic.websocket import AsyncJsonWebsocketConsumer
+    from channels.layers import get_channel_layer
+    from channels.db import database_sync_to_async
+    from asgiref.sync import async_to_sync
 
+    # create channels v1 decorators so that imports don't break
+    def channel_session_user(func):
+        def wrapper():
+            raise Exception("[django-nyt:consumers] Channels 2 detected: {} function should not be called".format(func))
+        return wrapper
+    
+    def channel_session_user_from_http(func):
+        def wrapper():
+            raise Exception("[django-nyt:consumers]Channels 2 detected: {} function should not be called".format(func))
+        return wrapper
+
+"""
+Channels version 1 consumer functions
+"""
 def get_subscriptions(message):
     """
     :return: Subscription query for a given message's user
@@ -16,7 +38,6 @@ def get_subscriptions(message):
         return models.Subscription.objects.filter(settings__user=message.user)
     else:
         return models.Subscription.objects.none()
-
 
 @channel_session_user_from_http
 def ws_connect(message):
@@ -55,3 +76,60 @@ def ws_receive(message):
     """
     logger.debug("Received a message, responding with a non-API message")
     message.reply_channel.send({'text': 'OK'})
+
+"""
+Channels version 2 consumer class
+"""
+class NYTconsumer(AsyncJsonWebsocketConsumer):
+     
+    async def connect(self):
+        """
+        Connected to websocket
+        """
+        logger.debug("Adding new connection for user {}".format(self.scope["user"]))
+
+        await self.channel_layer.group_add(
+            settings.NOTIFICATION_CHANNEL.format(
+                notification_key=subscriptions.notification_type.key
+            ),
+            self.channel_name
+        )
+        
+        await self.accept()
+
+    async def disconnect(self):
+        """
+        Websocket closed
+        """
+
+        await self.channel_layer.group_discard(
+            settings.NOTIFICATION_CHANNEL.format(
+                notification_key=subscriptions.notification_type.key
+            ),
+            self.channel_name
+        )
+
+    async def receive_json(self, content):
+        """
+        Received a message. Do nothing.
+        """
+        pass
+
+    async def notification(self, event):
+        """
+        Sending notification text
+        """
+
+        await self.send_json({
+            'text': event['text'],
+        })
+
+    #async def get_subscriptions(self):
+    #    """
+    #    :return: Subscription query for a given message's user
+    #    """
+    #    if self.scope["user"].is_authenticated:
+    #        subcriptions = await database_sync_to_async(models.Subscription.objects.filter(settings__user=self.scope["user"]))()
+    #    else:
+    #        subcriptions = await database_sync_to_async(models.Subscription.objects.none())()
+    #    return subscriptions
--- django_nyt/subscribers.py
+++ django_nyt/subscribers.py
@@ -1,11 +1,39 @@
 import logging
 
-from channels import Group
+try:
+    channels_ver = "1"
+    from channels import Group
+except Exception as e:
+    logger.info("Not using channels 1. Attempting channels 2...")
+    channels_ver = "2"
+    from channels.layers import get_channel_layer
+    from asgiref.sync import async_to_sync
 
 from . import models, settings
 
 logger = logging.getLogger(__name__)
 
+def notify_via_channels_v1(notification_type):
+    g = Group(
+        settings.NOTIFICATION_CHANNEL.format(
+            notification_key=notification_type['key']
+        )
+    )
+    g.send(
+        {'text': 'new-notification'}
+    )
+
+def notify_via_channels_v2(notification_type):
+    channel_layer = get_channel_layer()
+    async_to_sync(channel_layer.group_send)(
+        settings.NOTIFICATION_CHANNEL.format(
+            notification_key=notification_type['key']
+        ),
+        {
+            'type': 'notification',
+            'text': 'new-notification',
+        },
+    )
 
 def notify_subscribers(notifications, key):
     """
@@ -17,11 +45,10 @@ def notify_subscribers(notifications, key):
     notification_type_ids = models.NotificationType.objects.values('key').filter(key=key)
 
     for notification_type in notification_type_ids:
-        g = Group(
-            settings.NOTIFICATION_CHANNEL.format(
-                notification_key=notification_type['key']
-            )
-        )
-        g.send(
-            {'text': 'new-notification'}
-        )
+        
+        if channels_ver == "1":
+            notify_via_channels_v1(notification_type)
+        elif channels_ver == "2":
+            notify_via_channels_v2(notification_type)
+        else:
+            raise Exception("[django-nyt:subscribers] Unknown channels version")
